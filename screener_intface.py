181baab792f0ebc85cd8599f71437fcd92b47a24
carlamHS <88361302+carlamHS@users.noreply.github.com>
2022-08-19 17:02:46 +0800

    Add files via upload

--- screener_intface.py
+++ screener_intface.py
@@ -128,9 +128,10 @@ buy_at = st.sidebar.text_input('Enter your buy at price', '100')
 
 stop_loss = st.sidebar.text_input('Enter your stop loss','99')
 
+import bokeh 
 from bokeh.models.widgets import Div
 import streamlit as st
-import bokeh 
+
 if st.button('Go to Streamlit'):
     js = "window.open('https://www.streamlit.io/')"  # New tab or window
     js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
